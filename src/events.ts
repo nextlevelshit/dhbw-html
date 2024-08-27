10dcc8e7fd151518e18539b643d157292472df50
Michael Czechowski <m.czechowski@pons.de>
2024-08-28 00:15:22 +0200

    WIP

--- src/events.ts
+++ src/events.ts
@@ -4,33 +4,9 @@ export default () => {
   sectionList.forEach(section => {
     section.addEventListener('click', (e) => {
       console.log('click', e.target);
-
-      e.srcElement
-
     });
     section.addEventListener('touchstart', (e) => {
       console.log('touchstart', e.target);
     });
   });
-}
-
-
-// export class Events {
-
-//   private sectionList: NodeListOf<HTMLElement>;
-
-//   constructor () {
-//     this.sectionList = document.querySelectorAll('section');
-//   }
-
-//   public listen() {
-//     this.sectionList.forEach(section => {
-//       section.addEventListener('click', (e) => {
-//         console.log('click', e.target);
-//       });
-//       section.addEventListener('touchstart', (e) => {
-//         console.log('touchstart', e.target);
-//       });
-//     });
-//   }
-// }
+};
